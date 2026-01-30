b4f72164db853dc9c53850bac70dd64e44ccb66c
zaapp0013 <101802973+zaapp0013@users.noreply.github.com>
2026-01-30 19:49:14 +0930

    Adding generic char script for anim  player and sprite access

---
+++ Jericho/char.gd
@@ -0,0 +1,8 @@
+class_name Char
+extends Node2D
+
+@onready var sprite_2d: Sprite2D = %Sprite2D
+@onready var animation_player: AnimationPlayer = %AnimationPlayer
+
+func play_anim(anim_name: String) -> void:
+	animation_player.play(anim_name)
